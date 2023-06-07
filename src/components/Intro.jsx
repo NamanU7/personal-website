45a9eff6e6395888aec0da3e44bfac951506d00d
Naman Uttamchandani <naman.uttam7@gmail.com>
2023-06-07 16:32:16 -0400

    Minor changes in about me section

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -10,19 +10,19 @@ function Intro() {
         Student
       </p>
       <p className="text-sm max-w-xl mb-4 font-bold dark:text-stone-100">
-        Just a guy who was born and raised on a faraway island called Curacao.
+        Just a guy who was born and raised on a faraway island called Curaçao.
         Whom by the grace of God has the opportunity to study Computer Science
         at The University of British Columbia. My academic interests include Web
         development (specifically backend), system-level programming such as OS
         development and compiler construction (BTW I use Rust), interested in
-        WASM and its potential, and dabbling and eager to learn more about
-        Machine Learning. I am curious and eager to build and learn more things
-        in this field. Other interests include: Swimming (I am from an island so
-        this is a given), music (I'm learning the guitar), biking, and the gym.
-        Below is a display of some of the projects I have/am working on. Thanks
-        for dropping by! I would love to talk to you, just scroll down and leave
-        a message, I will try to reply fast! Oh, and did I tell you? I use Rust
-        by the way 😂😂😂!
+        WASM and its potential. Dabbling in and very eager to learn more about
+        machine learning. I am curious and excited to build and learn more
+        things in this field. Other interests include: Swimming (I am from an
+        island so this is a given), music (I'm learning the guitar), biking, and
+        the gym. Below is a display of some of the projects I have/am working
+        on, feel free to check them out! Thanks for dropping by! I would love to
+        talk to you, just scroll down and leave a message, I will try to reply
+        ASAP! Oh, and did I tell you?.....By the way.....I use Rust 😂😂😂!
       </p>
     </div>
   );
