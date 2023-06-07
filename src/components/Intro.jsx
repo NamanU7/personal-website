986f7a60fc18ce747de9616191121cc7f10e056a
Naman Uttamchandani <naman.uttam7@gmail.com>
2023-06-06 21:53:20 -0400

    Finish about paragraph & add iron_chip project
    Finished up the about me paragraph which can still be modified in the
    future. I also replaced the quick news project with the iron_chip one to
    to have a bit of a balance and show some rust.

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,18 +1,31 @@
-import React from 'react';
+import React from "react";
 
 function Intro() {
-    return (
-        <div className="flex items-center justify-center flex-col text-center pt-20 pb-10">
-            <h1 className="text-4xl md:text-7xl mb-4 md:mb-8 font-bold">Naman Uttamchandani</h1>
-            <p className="text-base md:text-xl mb-5 font-medium dark:text-stone-100">Student</p>
-            <p className="text-sm max-w-xl mb-4 font-bold dark:text-stone-100">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean porta sagittis lacus, eu pharetra erat porttitor ut.
-                Donec rutrum sollicitudin accumsan. Sed ut urna vel magna egestas dictum. Aenean urna nisi, mollis quis neque eu, vulputate vehicula tortor.
-                Fusce eu leo in nulla posuere vehicula. Morbi suscipit eros at est vestibulum, a ullamcorper lectus mattis.
-                Maecenas pharetra suscipit urna, ac congue odio dictum commodo. Fusce elementum consequat leo a finibus. In ultrices vel dolor eu ultricies. (TBA)
-            </p>
-        </div>
-    );
+  return (
+    <div className="flex items-center justify-center flex-col text-center pt-20 pb-10">
+      <h1 className="text-4xl md:text-7xl mb-4 md:mb-8 font-bold">
+        Naman Uttamchandani
+      </h1>
+      <p className="text-base md:text-xl mb-5 font-medium dark:text-stone-100">
+        Student
+      </p>
+      <p className="text-sm max-w-xl mb-4 font-bold dark:text-stone-100">
+        Just a guy who was born and raised on a faraway island called Curacao.
+        Whom by the grace of God has the opportunity to study Computer Science
+        at The University of British Columbia. My academic interests include Web
+        development (specifically backend), system-level programming such as OS
+        development and compiler construction (BTW I use Rust), interested in
+        WASM and its potential, and dabbling and eager to learn more about
+        Machine Learning. I am curious and eager to build and learn more things
+        in this field. Other interests include: Swimming (I am from an island so
+        this is a given), music (I'm learning the guitar), biking, and the gym.
+        Below is a display of some of the projects I have/am working on. Thanks
+        for dropping by! I would love to talk to you, just scroll down and leave
+        a message, I will try to reply fast! Oh, and did I tell you? I use Rust
+        by the way 😂😂😂!
+      </p>
+    </div>
+  );
 }
 
 export default Intro;
--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,22 +1,22 @@
 const projectData = [
-    {
-        title: "Impostor",
-        imgUrl: "",
-        techStack: ["HTML", "CSS", "Node.js", "Express.js", "Socket.io", "pug"],
-        link: "TBA"
-    },
-    {
-        title: "Sola.taire",
-        imgUrl: "/assets/solataire-display.jpg",
-        techStack: ["HTML", "React", "Node.js", "Express.js"],
-        link: "https://solataire.netlify.app/"
-    },
-    {
-        title: "Quick News",
-        imgUrl: "/assets/quick-news-display.jpg",
-        techStack: ["HTML", "CSS", "JavScript"],
-        link: "https://namanu7.github.io/quick-news/"
-    }
+  {
+    title: "Impostor",
+    imgUrl: "",
+    techStack: ["HTML", "CSS", "Node.js", "Express.js", "Socket.io", "pug"],
+    link: "TBA",
+  },
+  {
+    title: "Sola.taire",
+    imgUrl: "/assets/solataire-display.jpg",
+    techStack: ["HTML", "Node.js", "Express.js"],
+    link: "https://solataire.netlify.app/",
+  },
+  {
+    title: "Iron Chip",
+    imgUrl: "",
+    techStack: ["Rust", "SDL2", "WASM"],
+    link: "https://github.com/NamanU7/iron_chip",
+  },
 ];
 
 export default projectData;
