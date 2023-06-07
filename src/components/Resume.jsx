208bdbe63f4fbb45a3e1ff45a50828eb75f5df60
Naman Uttamchandani <naman.uttam7@gmail.com>
2023-06-07 10:38:54 -0400

    Resume now downloadable and minor changes
    Instead of displaying the resume, you can now download it, fits better
    with the overall flow of the page.

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,20 +1,30 @@
-import React from 'react';
-import Title from './Title';
-import { Document, Page } from 'react-pdf/dist/esm/entry.vite';
+import React from "react";
+import Title from "./Title";
+import { Document, Page } from "react-pdf/dist/esm/entry.vite";
 
 function Resume() {
-    return (
-        <div className="flex flex-col justify-center items-center my-20">
-            <Title>Resume</Title>
+  return (
+    <div className="flex flex-col justify-center items-center my-20">
+      <Title>Resume</Title>
 
-            <Document file="/assets/resume.pdf">
-                <Page size="A4" pageIndex={0} />
-            </Document>
+      {/* <Document file="/assets/resume.pdf"> */}
+      {/* <PDFViewer /> */}
+      {/* <Page size="A4" pageNumber={1} /> */}
+      {/* </Document> */}
 
-            <button>Download</button>
-
-        </div>
-    );
+      <button>
+        <a
+          className="text-center inline-block px-8 py-3 w-max text-base font-medium rounded-md
+                        text-white drop-shadow-xl bg-gradient-to-r from-sky-400 bg-emerald-400 
+                        hover:outline-amber-400"
+          href="/assets/resume.pdf"
+          download="Resume_Naman_Uttamchandani.pdf"
+        >
+          Download Resume
+        </a>
+      </button>
+    </div>
+  );
 }
 
 export default Resume;
