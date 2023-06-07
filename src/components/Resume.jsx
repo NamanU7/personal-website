7659c409b9209a7aa7c95b07e179b77a28a4d3e9
Naman Uttamchandani <naman.uttam7@gmail.com>
2023-06-07 10:50:06 -0400

    Remove pdf-renderer dependency

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,16 +1,10 @@
 import React from "react";
 import Title from "./Title";
-import { Document, Page } from "react-pdf/dist/esm/entry.vite";
 
 function Resume() {
   return (
     <div className="flex flex-col justify-center items-center my-20">
       <Title>Resume</Title>
-
-      {/* <Document file="/assets/resume.pdf"> */}
-      {/* <PDFViewer /> */}
-      {/* <Page size="A4" pageNumber={1} /> */}
-      {/* </Document> */}
 
       <button>
         <a
